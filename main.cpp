3f85fd492f071504fb6f0b1b9d09384a84f8846a
spunky13chick <miss.phe.wagner@gmail.com>
2015-03-04 17:21:36 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,113 @@
-#import<iostream>
-char BinSrchTree::miniMenu(bool isMiniMenu)
-//A menu that prints options, and takes the user's choice
-{
-    char choice;
-    if (isMiniMenu){
-    cout << endl << "What would you like to do?" << endl;
-    cout << endl;
-    cout << "\t1) Build a binary search tree." << endl;
-    cout << "\t2) Traverse the tree using InOrder." << endl;
-    cout << "\t3) Traverse the tree using LevelOrder." << endl;
-    cout << "\t4) Traverse the tree using PostOrder." << endl;
-    cout << "\t5) Delete a specific node." << endl;
-    cout << "\t6) Clear the tree." << endl;
-    cout << "\t7) Run the case specified in the homework." << endl;
-    cout << endl << "\t0) Quit." << endl;
-    }
-    else
-    {
-        cout << "\n\n1)Build\t\t2)InOrder\t3)LevelOrder\t4)PostOrder\t5)Delete\t\t6)Clear\t\t7)HWCase\n";
-    }
-    cout << endl;
-    cout << ">>>";
-    //choice = getchar();
-    cin >> choice;
-    cout << endl;
-    return choice;
-}
-int BinSrchTree::menu()
-{
+#include <iostream>
 
-    char option = NULL;
-    bool printMenu = true;
-    cout <<"            ,@@@@@@@,\n"
-    <<"    ,,,.   ,@@@@@@/@@,  .oo8888o.\n"
-    <<" ,&%%&%&&%,@@@@@/@@@@@@,8888\\88/8o\n"
-   <<",%&\\%&&%&&%,@@@\\@@@/@@@88\\88888/88'\n"
-   <<"%&&%&%&/%&&%@@\\@@/ /@@@88888\\88888'\n"
-   <<"%&&%/ %&%%&&@@\\ \V /@@' `88\\8 `/88'\n"
-   <<"`&%\\ ` /%&'    |.|        \\ '|8'\n"
-   <<"    |o|        | |         | |\n"
-   <<"    |.|        | |         | |\n"
-   <<" _\\/ ._\\/____ /__ \\_//__\\\\/___\\_//__/_*/ ____\n"
-    <<"( \\/\\/ )( ___)(  )  / __)(  _  )(  \\/  )( ___)\n"
-    <<" )    (  )__)  )(__( (__  )(_)(  )    (  )__) \n"
-    <<"(__/\\__)(____)(____)\\___)(_____)(_/\\/\\_)(____)\n";
+#include <sys/stat.h>
+#include <ctime>
+#include <stdint.h>
 
-    while(option!='0')
-    {
-        option = miniMenu(printMenu);
-        switch(option)
-        {
-            case '0': cout << "  _____             ____\n"
-                << " / ___/__  ___  ___/ / /  __ _____ \n"
-                << "/ (_ / _ \\/ _ \\/ _  / _ \\/ // / -_)\n"
-                << "\\___/\\___/\\___/\\_,_/_.__/\\_, /\\__/ \n"
-                << "                        /___/      \n";
-            break;
-            case '1':
-                int n;
-                cout << "Enter how many integers you would like to enter into the tree: ";
-                cin >> n;
-                for(int i=1; i<=n; i++)
-                    {
-                        fillNode();
-                        insertNode();
-                    }
-                printMenu = false;
-                break;
-            case '2': inOrder(); printMenu = false; break;
-            case '3': levelOrder(); printMenu = false; break;
-            case '4': postOrder(); printMenu = false; break;
-            case '5':
-                int del;
-                cout << "Please enter the data value of the node you would like to delete: ";
-                cin >> del;
-                deleteNode(del);
-                printMenu = false;
-                break;
-            case '6': clearTree(); printMenu = false; break;
-            case '7':
-                cout << "Clearing existing trees and executing the homework case." << endl;
-                break;
-            default : cerr << "\nERROR: '" << option << "' is not a valid menu option.\n\n"; printMenu = true; break;
-        }
-    }
-    return(0);
+#define DATA_FILE       "./intbits.txt"
+
+
+using namespace std;
+
+
+extern "C"
+{
+	__inline__ uint64_t rdtsc()
+	{
+		uint32_t lo, hi;
+		/* We cannot use "=A", since this would use %rax on x86_64 */
+		__asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+
+		return (uint64_t)hi << 32 | lo;
+	}
 }
+
 
 int main()
 {
-    BinSrchTree a;
-   a.menu();
+    // get size of data file
+    //
+	struct stat st;
+	int status = stat(DATA_FILE, &st);
+	if (status < 0)
+    {
+        cout << "Cannot stat " << DATA_FILE << "\n";
+        exit(0);
+    }
 
+    // we need at least 28 chars for the pattern and some following
+    //
+    if (st.st_size < 29)
+    {
+        cout << "Size of data file is too small.\n";
+        exit(0);
+    }
+
+    // allocate array for data and read it in
+    //
+    char *pData = new char[st.st_size];
+
+    FILE *fp = fopen(DATA_FILE, "r");
+    if (!fp)
+    {
+        cout << "Cannot open " << DATA_FILE << "\n";
+        exit(0);
+    }
+
+    fread(pData, st.st_size, 1, fp);
+    fclose(fp);
+
+
+    //  scan data starting 1 byte in
+    //
+    uint64_t tStart = rdtsc();
+
+    int matches = 0;
+
+
+for(int p = 0; p < 1000000; p++)
+{
+    for (int i = 1; i < st.st_size-28; i++)
+    {
+        bool bFound = true;
+        for (int j = 0; j < 28; j++)
+        {
+            if (pData[j] != pData[i+j])
+            {
+                bFound = false;
+                break;
+            }
+        }
+        if (!bFound)
+            continue;
+
+        // found a match; set high bit to mark it
+        //
+        pData[i] |= 0x80;
+        matches++;
+    }
+
+}
+
+    uint64_t delta = (rdtsc() - tStart) / 1000000;
+
+
+    cout << "Time: " << delta << " uSec, Matches: " << matches << "\n";
+
+    for (int i = 1; i < st.st_size-28; i++)
+    {
+        if (!(pData[i] & 0x80))
+            continue;
+
+        // print match location
+        //
+        cout << "  Match: " << i << "\n";
+    }
+
+
+
+
+
+
+    return 0;
 }
